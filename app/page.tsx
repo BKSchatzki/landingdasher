97ad82dbdd3f93bb0c4d2d9214d93125f3e4c6b2
BKSchatzki <106890912+BKSchatzki@users.noreply.github.com>
2024-01-12 11:04:48 -0800

    Update clerk to fix security issue

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
-import Image from "next/image";
+import { SignIn } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div className="flex min-h-screen min-w-full items-center justify-center">
       <span>Hello World</span>
+      <SignIn />
     </div>
   );
 }
